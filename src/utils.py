59627338b25c19961064ef1f3f495f66c20fcda2
TakatoYoshikawa <tenten0727@icloud.com>
2021-12-28 23:42:34 +0900

    weighted_correlation

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def weighted_correlation(a, b, weights):
+
+  w = np.ravel(weights)
+  a = np.ravel(a)
+  b = np.ravel(b)
+
+  sum_w = np.sum(w)
+  mean_a = np.sum(a * w) / sum_w
+  mean_b = np.sum(b * w) / sum_w
+  var_a = np.sum(w * np.square(a - mean_a)) / sum_w
+  var_b = np.sum(w * np.square(b - mean_b)) / sum_w
+
+  cov = np.sum((a * b * w)) / np.sum(w) - mean_a * mean_b
+  corr = cov / np.sqrt(var_a * var_b)
+
+  return corr
